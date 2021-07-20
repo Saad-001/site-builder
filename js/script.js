0722ee870894d63b10aa1697c9847f228c482ac1
Saad-001 <saadafnan21@gmail.com>
2021-07-20 09:26:48 +0600

    sixth commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+$('.owl-carousel').owlCarousel({
+  loop: false,
+  margin: 10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      769:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
+
 // grab everything we need
 const btn = document.querySelector(".mobile-menu-button");
 const sidebar = document.querySelector(".sidebar");
