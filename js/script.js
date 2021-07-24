028d151d457a474226cda3794b41c7a0ac619da2
Saad-001 <saadafnan21@gmail.com>
2021-07-24 11:38:47 +0600

    one issue fixed

--- js/script.js
+++ js/script.js
@@ -23,4 +23,5 @@ const sidebar = document.querySelector(".sidebar");
 // add our event listener for the click
 btn.addEventListener("click", () => {
   sidebar.classList.toggle("-translate-x-full");
+  sidebar.classList.add("z-10");
 });
